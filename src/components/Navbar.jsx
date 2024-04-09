fa9049ff5c10fb1ef961600054c0df095eef2834
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-04-09 06:43:16 -0700

    ---

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,20 +6,20 @@ import Logo from "../assets/logo.png";
 
 export default function Navbar(){
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-    const [navbarColor, setNavbarColor] = useState("transparent");
+    const [navbarColor, setNavbarColor] = useState("bg-[transparent]");
     const location = useLocation();
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY >= 50) {
-                setNavbarColor("#000");
+                setNavbarColor("alternative-nav-color");
                 
             } else {
                 if (location.pathname === '/about') {
                     console.log("Setting navbar color to #000");
-                    setNavbarColor("#000");
+                    setNavbarColor("alternative-nav-color");
                 } else {
                     console.log("Setting navbar color to transparent");
-                    setNavbarColor("transparent");
+                    setNavbarColor("bg-[transparent]");
                 }
             }
         };
@@ -28,10 +28,10 @@ export default function Navbar(){
         console.log("Current pathname:", location.pathname);
         if (location.pathname === '/about') {
             console.log("Setting navbar color to #000");
-            setNavbarColor("#000");
+            setNavbarColor("alternative-nav-color");
         } else {
             console.log("Setting navbar color to transparent");
-            setNavbarColor("transparent");
+            setNavbarColor("bg-[transparent]");
         }
 
         window.addEventListener("scroll", handleScroll);
@@ -44,7 +44,7 @@ export default function Navbar(){
    
     return(
         <>
-        <nav className={`snf-navbar z-20 h-[90px] fixed w-full left-[0] bg-[${navbarColor}] lg:px-[50px] px-[20px] flex items-center justify-between`}>
+        <nav className={`snf-navbar z-20 h-[90px] fixed w-full left-[0] ${navbarColor} lg:px-[50px] px-[20px] flex items-center justify-between`}>
             <div className="w-[100px]">
                 <HashLink to="/">
                     <img className="max-w-[100px] w-full block" src={Logo} alt="" />
@@ -57,10 +57,10 @@ export default function Navbar(){
                 <HashLink className="inline-block py-[0.3rem] px-[0.6rem] text-[#fff] no-underline my-[0] mx-[15px] lg:text-[95%] text-[120%] font-medium" onClick={handleMobileMenuToggle} to="#contact">Contact</HashLink>
             </div>
             <div className="lg:flex hidden items-center">
-                <div className="px-[2.5rem] py-[1rem] bg-[#fff] rounded-[100px]">
+                <div className="px-[2.5rem] py-[1rem] bg-[#fff] get-started rounded-[100px]">
                     <p>Get started</p>
                 </div>
-                <div className="w-[50px] h-[50px] ml-[-5px] flex justify-center items-center py-[1rem] bg-[#fff] rounded-[50%]">
+                <div className="w-[50px] h-[50px] get-started-circle ml-[-5px] flex justify-center items-center py-[1rem] bg-[#fff] rounded-[50%]">
                     <MoveUpRight />
                 </div>
             </div>
