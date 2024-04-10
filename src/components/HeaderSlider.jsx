d82f2b902157496f47e5392a885845da047bebb1
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-04-09 18:36:52 -0700

    ---

--- src/components/HeaderSlider.jsx
+++ src/components/HeaderSlider.jsx
@@ -51,13 +51,13 @@ export default function HeaderSlider(){
             </SwiperSlide>
             <SwiperSlide>
                 <div className="p-[1rem] rounded-[1rem] glass-bg flex items-center">
-                    <img className='w-[180px] h-[120px] rounded-[1rem]' src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
+                    <img className='w-[180px] h-[120px] rounded-[1rem]' src="https://img.freepik.com/free-photo/green-beans-spices-green-beans-bowl-white-notebook-bowls-spices-tomatoes-with-stalks-dark-table_140725-120153.jpg?t=st=1712712878~exp=1712716478~hmac=39d268371b9d1299f20e36603a64261f74e267bc127f9fec74cc572338a067ff&w=740" alt="" />
                     <h3 className='text-[100%] w-[40%] ml-[1rem] font-medium'>Share delicious recipes in video form.</h3>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div className="p-[1rem] rounded-[1rem] glass-bg flex items-center">
-                    <img className='w-[180px] h-[120px] rounded-[1rem]' src="https://img.freepik.com/free-photo/ecofriendly-fresh-market-produce_482257-76147.jpg?size=626&ext=jpg&ga=GA1.2.3493108.1707066373&semt=ais" alt="" />
+                    <img className='w-[180px] h-[120px] rounded-[1rem]' src="https://img.freepik.com/free-photo/side-view-woman-garage-sale_23-2150577065.jpg?t=st=1712712936~exp=1712716536~hmac=a64078abee8849c84785eecaad7adb2142a8a2fd69b4222c396897967af097f8&w=740" alt="" />
                     <h3 className='text-[100%] w-[40%] ml-[1rem] font-medium'>Connect users with quality foodstuffs vendors.</h3>
                 </div>
             </SwiperSlide>
